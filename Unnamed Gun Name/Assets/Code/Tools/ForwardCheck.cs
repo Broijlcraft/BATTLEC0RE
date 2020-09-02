7d7d4f7fd64587d3cfe8a73fa31c9ff77e039cb7
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-09-02 09:15:36 +0200

    Rocket
    - Add DevAmmo_RPG.fbx
    - Add ForwardCheck.cs

---
+++ Unnamed Gun Name/Assets/Code/Tools/ForwardCheck.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForwardCheck : MonoBehaviour {
+
+    private void OnDrawGizmosSelected() {
+        Debug.DrawRay(transform.position, transform.forward, Color.red);
+    }
+}
