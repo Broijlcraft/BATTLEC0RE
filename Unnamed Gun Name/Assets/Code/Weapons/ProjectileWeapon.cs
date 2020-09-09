3551236e68199800ed126e201f04ad53b760ec12
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-09-09 09:50:34 +0200

    Smoll in scene fix

--- Unnamed Gun Name/Assets/Code/Weapons/ProjectileWeapon.cs
+++ Unnamed Gun Name/Assets/Code/Weapons/ProjectileWeapon.cs
@@ -6,8 +6,7 @@ public class ProjectileWeapon : FireArms {
 
     public string projectileName;
     public float projectileSpeed;
-
-
+    
     public override void ShootBehaviour(WeaponBehaviour behaviour, Transform attackOrigin) {
         Vector3 attackRotation = GetAttackRotation(attackOrigin, behaviour.range);
         GameObject projObject = ObjectPooler.single_OP.SpawnFromPool(projectileName, attackOrigin.position, Quaternion.Euler(attackRotation));
