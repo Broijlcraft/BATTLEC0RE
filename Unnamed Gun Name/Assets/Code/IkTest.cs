7cb748f0433baab4bf2f8260b8aec62c023ea0e0
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-09-17 18:48:10 +0200

    Ik test script

--- Unnamed Gun Name/Assets/Code/IkTest.cs
+++ Unnamed Gun Name/Assets/Code/IkTest.cs
@@ -2,17 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IkTest : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class IkTest : MonoBehaviour {
+    Animator anim;
+
+    public float speed;
+    [Range(0, 1)]
+    public float distanceToGround;
+
+    public LayerMask ignoreLayers;
+
+    private void Awake() {
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        float fw = Input.GetAxis("Vertical");
+        if (fw != 0) { 
+            anim.SetBool("Walk", true);
+        } else {
+            anim.SetBool("Walk", false);
+        }
+        transform.Translate(transform.forward*fw*Time.deltaTime*speed);
+    }
+
+    private void OnAnimatorIK(int layerIndex) {
+        print("anim");
+        if (anim) {
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+
+
+            RaycastHit hit;
+            print(anim.GetIKPosition(AvatarIKGoal.LeftFoot));
+            Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+
+            if (Physics.Raycast(ray, out hit, distanceToGround + 1f, ignoreLayers)) {
+                print(hit.transform.name);
+                if (hit.transform.tag == "Walkable") {
+                    Vector3 footPosition = hit.point;
+                    footPosition.y += distanceToGround;
+                    anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
+                    anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.LookRotation(transform.forward, hit.normal));
+                }
+            }
+            Debug.DrawRay(ray.origin, ray.direction);
+            print("ues");
+        }
     }
 }
