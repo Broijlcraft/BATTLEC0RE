7dd83268ba81a8c1fd22f8efd253827149a2e28c
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-09-20 10:20:35 +0200

    Fixed "small" Coroutine issuea

--- Unnamed Gun Name/Assets/Code/UI/KillFeedListing.cs
+++ Unnamed Gun Name/Assets/Code/UI/KillFeedListing.cs
@@ -7,28 +7,42 @@ public class KillFeedListing : MonoBehaviour {
     public Image weapon, headshot;
     public Text killerName, victimName;
     public Animator animator;
-    public float maxTimeOnScreen = 5f, timeToFadeOut = 1f;
+    public float timeBeforeFade = 5f, timeToFadeOut = 1f;
+
+    bool isActive;
+    float timer;
+
+    private void Update() {
+        if (isActive) {
+            timer += Time.deltaTime;
+            if (timer > timeBeforeFade) {
+                animator.ResetTrigger("Default");
+                animator.SetTrigger("FadeOut");
+                print("fade");
+                timer = 0;
+                isActive = false;
+            }
+        }
+    }
 
     public void SetKill(string killer, string victim, Sprite weaponIcon, Sprite headshotIcon, bool isHeadshot) {
-        StopCoroutine(Hide());
         gameObject.SetActive(true);
-        animator.ResetTrigger("FadeOut");
-        animator.SetTrigger("Default");
+        ResetFadeOut();
         transform.SetAsLastSibling();
         headshot.gameObject.SetActive(isHeadshot);
         weapon.sprite = weaponIcon;
         headshot.sprite = headshotIcon;
         killerName.text = killer;
         victimName.text = victim;
-        StartCoroutine(Hide());
+        
+        isActive = true;
     }
 
-    IEnumerator Hide() {
-        yield return new WaitForSeconds(maxTimeOnScreen);
-        FadeOut();
-    }
-
-    void FadeOut() {
-        animator.SetTrigger("FadeOut");
+    void ResetFadeOut() {
+        print("reset");
+        isActive = false;
+        timer = 0;
+        animator.ResetTrigger("FadeOut");
+        animator.SetTrigger("Default");
     }
 }
