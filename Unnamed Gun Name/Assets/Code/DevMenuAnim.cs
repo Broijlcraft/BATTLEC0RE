c50b8a6ca9fa2a9c5ea79d6065a7a62353ffe4b4
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-10-06 13:07:03 +0200

    WIP

---
+++ Unnamed Gun Name/Assets/Code/DevMenuAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevMenuAnim : MonoBehaviour {
+
+    public Animator door, bot, mine;
+
+    [Header("Door")]
+    public bool open, close;
+    bool hasBeenOpen, hasBeenClosed;
+
+    [Header("Bot")]
+    public bool dropFromTop, dropFromDoor, land, walk;
+    bool wasDroppedFromTop, wasDroppenFromTop, hasLanded, hasWalked;
+
+    private void Update() {
+        if (open && !hasBeenOpen) {
+            door.SetTrigger("Open");
+            open = false;
+        } else if(!open && hasBeenOpen) {
+            hasBeenOpen = false;
+        }
+    }
+}
