1e1b21d3c770056068f70a3e50d772faac598426
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-10-19 16:57:29 +0200

    quick safe

--- Unnamed Gun Name/Assets/Code/RobotParts/BodyPartsManager.cs
+++ Unnamed Gun Name/Assets/Code/RobotParts/BodyPartsManager.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 
 public class BodyPartsManager : MonoBehaviour {
     public List<CustomRobotPart> customRobotParts = new List<CustomRobotPart>();
+
+    [Space(30)]
+    public Material normalMat;
+    public Material normalMatEm;
+    public Material heavyMat;
+    public Material heavyMatEm;
+
+    private void Start() {
+        //for (int i = 0; i < 20; i++) {
+        //    CustomRobotPart part = new CustomRobotPart();
+        //    part.bodypartType = (BodypartType)i;
+        //    for (int iB = 0; iB < 2; iB++) {
+        //        Material mat = normalMat;
+        //        if (iB == 1) {
+        //            mat = heavyMat;
+        //        }
+        //        MeshAndMats meshAndMats = new MeshAndMats();
+        //        meshAndMats.mats.Add(mat);
+        //        part.variants.Add(meshAndMats);
+        //    }
+        //    customRobotParts.Add(part);
+        //}
+    }
 }
 
 [Serializable]
 public class CustomRobotPart {
+    [HideInInspector] public string partName;
     public BodypartType bodypartType;
 
-    public MeshAndMats[] variants;
-}
-
-[Serializable]
-public class MeshAndMats {
-    public Mesh mesh;
-    public Material mat;
+    public List<Mesh> meshes = new List<Mesh>();
 }
